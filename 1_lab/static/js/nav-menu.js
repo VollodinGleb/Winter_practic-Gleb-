fd3a9e87567fb6639914f06c97d81e19c83b44ea
G L E B U S <glebvollodin@gmail.com>
2024-01-20 18:04:12 +0200

    Clean code

--- 1_lab/static/js/nav-menu.js
+++ 1_lab/static/js/nav-menu.js
@@ -1,8 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const navigationToggle = document.querySelector('.navigation-toggle');
-    const header = document.querySelector('header');
+    const ToggleButton = document.querySelector('.navigation-toggle');
+    const ResponsiveMenu = document.querySelector('.responsive-menu');
+    const MainNavMenu = document.querySelector('#main-nav-menu');
+    const NavContainer = document.querySelector('.nav-container');
+    var prevScrollpos = window.scrollY;
 
-    navigationToggle.addEventListener('click', function () {
-        header.classList.toggle('menu-open');
+    ToggleButton.addEventListener('click', function () {
+        ResponsiveMenu.classList.toggle('active');
     });
+
+    window.addEventListener('resize', function () {
+        if (window.innerWidth > parseInt(getComputedStyle(document.documentElement).getPropertyValue('--max-width-for-toggle'))) {
+            ResponsiveMenu.classList.remove('active');
+            NavContainer.style.display = '';
+        }
+    });
+    
+    window.addEventListener('scroll', function(){
+        var currentScrollPos = this.window.scrollY;
+
+        MainNavMenu.classList.toggle('scrolled', window.scrollY > 600);
+
+        if (prevScrollpos > currentScrollPos) {
+            MainNavMenu.style.top = "0";
+        }else {
+            MainNavMenu.style.top = "-100px";
+        }
+        prevScrollpos = currentScrollPos;
+    })
 });
