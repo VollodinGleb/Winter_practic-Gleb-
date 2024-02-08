60957028cf3f6388e2e94dacf596307829b8fbe0
G L E B U S <glebvollodin@gmail.com>
2024-02-08 12:25:21 +0200

    Use 'createUseStyles' from react-jss

--- 2_lab/react_vite_lab1/src/components/buttons/RadiusButton.jsx
+++ 2_lab/react_vite_lab1/src/components/buttons/RadiusButton.jsx
@@ -1,17 +1,31 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-const Button = styled.a`
-    display: inline-block;
-    padding: 15px 10px 15px;
-    min-width: 200px;
-    border-radius: 30px;
-    text-align: center;
-    background-color: #FCD980;
-    color: #000;
-`
+// const Button = styled.a`
+//     display: inline-block;
+//     padding: 15px 10px 15px;
+//     min-width: 200px;
+//     border-radius: 30px;
+//     text-align: center;
+//     background-color: #FCD980;
+//     color: #000;
+// `
+import {createUseStyles} from 'react-jss'
+
+const useStyles = createUseStyles({
+    radiusButton: {
+        display: 'inline-block',
+        padding: '15px 10px 15px',
+        minWidth: '200px',
+        borderRadius: '30px',
+        textAlign: 'center',
+        backgroundColor: '#FCD980',
+        color: '#000'
+    }
+})
 
 export default function RadiusButton({ href, text }) {
+    const classes = useStyles()
     return (
-        <Button href={href}>{text}</Button>
+        <a className={classes.radiusButton} href={href}>{text}</a>
     );
 }
