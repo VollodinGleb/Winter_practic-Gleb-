7f09bf2b67f90e774da488724419eab018b6c2be
G L E B U S <glebvollodin@gmail.com>
2024-02-11 19:25:29 +0200

    add: rename NavToggle

--- 2_lab/react_vite_lab1/src/components/header/headerComponents/NavBar.jsx
+++ 2_lab/react_vite_lab1/src/components/header/headerComponents/NavBar.jsx
@@ -1,6 +1,6 @@
 import {createUseStyles} from 'react-jss'
 import Menu from './Menu'
-import NavionToogle from './Navtoggle';
+import NavionToogle from './NavionToogle';
 
 const NavBarStyles = createUseStyles({
     mainNavMenu : {
